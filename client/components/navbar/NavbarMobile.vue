9654e316ba2c61b0273710253a2cf4f94bd8774b
Nicolás Miranda <nicxesh@gmail.com>
2021-12-29 12:27:24 -0300

    Added cart mobile

--- client/components/navbar/NavbarMobile.vue
+++ client/components/navbar/NavbarMobile.vue
@@ -1,10 +1,30 @@
 <template>
-  <nav class="bg-white border-b border-[#D9DBE9] px-4 py-3 flex items-center justify-between mb-6">
-    <Logo />
-    <div>
-      <button class="border border-[#D9DBE9] rounded-lg p-2">
-        <img src="/images/icons/menu.svg">
-      </button>
+  <div class="md:hidden mb-6">
+    <nav class="bg-white border-b border-[#D9DBE9] px-4 py-3 flex items-center justify-between">
+      <Logo />
+      <div>
+        <button class="border border-[#D9DBE9] rounded-lg p-2">
+          <img src="/images/icons/menu.svg">
+        </button>
+      </div>
+    </nav>
+
+    <div v-if="showCart" class="bg-primary-yellow text-white px-2 py-1 flex flex-col items-center justify-between">
+      <div class="flex items-center">
+        <img src="/images/icons/cart.svg" class="mr-1">
+        <span class="text-[13px] font-medium">Cotización acutal:</span>
+      </div>
+      <span class="text-sm font-semibold leading-[26px]">152 productos</span>
     </div>
-  </nav>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      showCart: true,
+    }
+  },
+}
+</script>
