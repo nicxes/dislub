1748f8c5281d4eb750bd1483adc1b8ce516c563e
Nicolás Miranda <nicxesh@gmail.com>
2021-12-29 12:28:15 -0300

    FIX: Radius size on carousel

--- client/components/home/Carousel.vue
+++ client/components/home/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <img src="https://www.dislub.com.ar/wp-content/uploads/2018/10/slide-auto-1.jpg" class="h-full w-full rounded-[32px]">
+    <img src="https://www.dislub.com.ar/wp-content/uploads/2018/10/slide-auto-1.jpg" class="h-full w-full rounded-2xl md:rounded-[32px]">
   </div>
 </template>
