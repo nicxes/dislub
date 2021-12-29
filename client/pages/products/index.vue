f5d04e63b8ef3cf91203fda5a85e651e18e510cc
Nicolás Miranda <nicxesh@gmail.com>
2021-12-29 13:00:06 -0300

    Products page responsive

--- client/pages/products/index.vue
+++ client/pages/products/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="px-6 pb-20">
-    <ul class="grid grid-cols-7 gap-6">
+    <ul class="grid grid-cols-2 md:grid-cols-7 gap-y-4 gap-x-5 md:gap-6">
       <CardProduct v-for="(product, i) in 30" :key="i" name="FORMULA 5W-30" image="images/products/1.png" />
     </ul>
   </section>
