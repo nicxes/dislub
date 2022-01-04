d82892bd35a72307792739d4ef455f78b7afbbe3
Nicolás Miranda <nicxesh@gmail.com>
2022-01-04 19:54:09 -0300

    Added navigation mobile responsive

--- client/components/navigation/NavigationMobile.vue
+++ client/components/navigation/NavigationMobile.vue
@@ -1,15 +1,30 @@
 <template>
-  <nav class="fixed bottom-0 left-0 right-0 w-full z-50 grid grid-cols-3 items-center gap-3 md:hidden bg-[#FCFCFC] border-t-2 border-line p-2 list-none">
+  <nav class="fixed bottom-0 left-0 right-0 w-full z-50 grid Item grid-flow-col items-center gap-3 md:hidden bg-[#FCFCFC] border-t-2 border-line p-2 list-none  overflow-x-auto">
     <li>
-      <NavigationMobileItem text="Inicio" to="/dashboard" icon="/images/icons/home.svg" />
+      <NavigationMobileItem text="Inicio" to="/dashboard" icon="home" />
     </li>
 
     <li>
-      <NavigationMobileItem text="Productos" to="/products" icon="/images/icons/bag.svg" />
+      <NavigationMobileItem text="Productos" to="/products" icon="bag" />
     </li>
 
     <li>
-      <NavigationMobileItem text="Cotizaciones" to="/orders" icon="/images/icons/attach.svg" />
+      <NavigationMobileItem text="Cotizaciones" to="/orders" icon="attach" />
+    </li>
+
+    <li>
+      <NavigationMobileItem text="Usuarios" to="/users" icon="users" />
+    </li>
+
+    <li>
+      <NavigationMobileItem text="Categorías" to="/categories" icon="categories" />
     </li>
   </nav>
 </template>
+
+<style scoped>
+  .Item {
+    grid-template-columns: repeat(auto-fill,minmax(111px,1fr));
+    grid-auto-columns: minmax(111px,1fr);
+  }
+</style>
--- client/pages/products/index.vue
+++ client/pages/products/index.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="px-4 md:px-6 pb-20">
+    <div class="grid grid-cols-4 gap-6 mb-9">
+      <div class="Search relative col-span-2">
+        <input v-model="search" type="search" placeholder="Buscá cualquier producto" class="w-full rounded-2xl border-2 border-line py-3 pl-16 pr-6 text-input-placeholder-color bg-[#FCFCFC] focus:shadow-none focus:outline-none focus:ring-0 focus:border-line transition duration-300 ease-out">
+      </div>
+    </div>
+
     <ul class="grid grid-cols-2 md:grid-cols-7 gap-y-4 gap-x-5 md:gap-6">
-      <CardProduct v-for="(product, i) in products" :key="i" :name="product.name" :image="`images/products/${randomInt(1, 4)}.png`" />
+      <CardProduct v-for="(product, i) in filteredProducts" :key="i" :name="product.name" :image="`images/products/${randomInt(1, 4)}.png`" />
     </ul>
   </section>
 </template>
@@ -12,7 +18,15 @@ export default {
   data () {
     return {
       products: [],
+      search: '',
     }
+  },
+  computed: {
+    filteredProducts () {
+      return this.products.filter((product) => {
+        return product.name.toLowerCase().match(this.search.toLowerCase())
+      })
+    },
   },
   mounted () {
     this.getProducts()
@@ -31,3 +45,18 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  .Search::before {
+    content: '';
+    position: absolute;
+    left: 24px;
+    top: 15px;
+    z-index: 10;
+
+    height: 24px;
+    width: 24px;
+
+    background: url('/images/icons/search.svg') no-repeat center center;
+  }
+</style>
