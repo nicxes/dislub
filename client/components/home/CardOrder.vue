a0f9509d7d0a3fb1ba951bfa7620f16548aee422
Nicolás Miranda <nicxesh@gmail.com>
2022-01-13 05:09:10 -0300

    Added date to home orders

--- client/components/home/CardOrder.vue
+++ client/components/home/CardOrder.vue
@@ -3,8 +3,10 @@
     <NuxtLink :to="'/orders/' + order.id">
       <article class="Card flex items-center justify-between p-4 md:p-6 border-2 border-[#EFF0F6] md:border-transparent md:hover:border-[#EFF0F7] rounded-2xl bg-white md:bg-transparent md:hover:bg-white transition duration-300 cursor-pointer">
         <div>
-          <h5 class="text-darked md:text-lg font-semibold mb-1 md:mb-2 cutted">Viernes 3 de Diciembre</h5>
-          <h6 class="text-[#6E7191] text-sm font-medium mb-2">3/12/2021, 14:58hs</h6>
+          <h5 class="text-darked md:text-lg font-semibold mb-1 md:mb-2 cutted">
+            {{ formatTitle }}
+          </h5>
+          <h6 class="text-[#6E7191] text-sm font-medium mb-2">{{ formatDate }}, {{ formatTime }}hs</h6>
 
           <p class="text-primary text-sm font-medium underline md:hidden">
             Ver detalle
@@ -31,6 +33,21 @@ export default {
       required: true,
     },
   },
+  computed: {
+    formatDate () {
+      return this.$moment(this.order.created_at).format('DD/MM/YYYY')
+    },
+    formatTime () {
+      return this.$moment(this.order.created_at).format('HH:mm')
+    },
+    formatTitle () {
+      const day = this.$moment(this.order.created_at).format('dddd')
+      const date = this.$moment(this.order.created_at).format('DD')
+      const month = this.$moment(this.order.created_at).format('MMMM')
+
+      return day + ' ' + date + ' de ' + month
+    },
+  },
 }
 </script>
 
@@ -46,5 +63,8 @@ export default {
       height: 1.2em;
       white-space: nowrap;
     }
+  }
+  .cutted:first-letter {
+    text-transform: uppercase;
   }
 </style>
