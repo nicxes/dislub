435b243078614f75686ebf3d8290f6829299abef
Nicolás Miranda <nicxesh@gmail.com>
2022-01-17 08:09:09 -0300

    Added orders

--- client/pages/orders/index.vue
+++ client/pages/orders/index.vue
@@ -64,7 +64,7 @@
         <div class="flex items-center justify-between mb-4">
           <div class="flex items-center">
             <img
-              src="https://scontent.fepa8-2.fna.fbcdn.net/v/t1.18169-9/22730494_1499839286771311_6711186237223725316_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ygORrRK8X0gAX-XECJX&_nc_ht=scontent.fepa8-2.fna&oh=00_AT_mnllb8pogoOM7TdI9R12xAeQCVs64FE3DupqHmzKb6A&oe=61E28664"
+              src="https://scontent.fepa8-2.fna.fbcdn.net/v/t1.18169-9/22730494_1499839286771311_6711186237223725316_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HZE0h1jJW44AX824IwW&_nc_ht=scontent.fepa8-2.fna&oh=00_AT8-7ITpA4PlJOSWZ5IrlCp707pqkKDwY61D9sSKxJ_Ymg&oe=620A1364"
               class="h-8 w-8 rounded-lg border-2 border-line bg-white p-1 mr-2"
             >
             <span class="text-darked text-lg font-semibold">
@@ -101,15 +101,33 @@
 
     <div v-else>
       <div class="mb-7">
-        <div class="mb-4">
+        <div class="flex items-center justify-between mb-4">
           <h5 class="text-darked text-lg font-semibold">
             Recientes
           </h5>
+
+          <div>
+            <button class="flex items-center px-3 py-2 text-primary font-semibold text-sm rounded-2xl border-2 border-line hover:bg-line transition duration-300 ease-out" @click="orderByNew = !orderByNew">
+              <img
+                src="/images/icons/arrow-top.svg"
+                class="mr-0 md:mr-2 transition duration-300 ease-out"
+                :class="{ 'rotate-180': !orderByNew }"
+              >
+
+              <span v-if="orderByNew" class="hidden md:inline">
+                Ver antiguos
+              </span>
+
+              <span v-else class="hidden md:inline">
+                Ver recientes
+              </span>
+            </button>
+          </div>
         </div>
 
         <transition name="fade">
-          <ul v-show="show" class="grid grid-cols-1 gap-4">
-            <OrderCardByDate v-for="order in orders" :key="order.id" :order="order" />
+          <ul class="grid grid-cols-1 gap-4">
+            <OrderCardByDate v-for="order in reverseOrders" :key="order.id" :order="order" />
           </ul>
         </transition>
       </div>
@@ -125,9 +143,24 @@ export default {
       orderByDate: true,
       orders: [],
       ordersByOrgs: [],
-      show: true,
-      show2: true,
+      orderByNew: true,
     }
+  },
+  computed: {
+    reverseOrders () {
+      const orders = this.orders
+
+      if (this.orderByNew)
+        orders.sort((a, b) => {
+          return new Date(b.created_at) - new Date(a.created_at)
+        })
+      else
+        orders.sort((a, b) => {
+          return new Date(a.created_at) - new Date(b.created_at)
+        })
+
+      return orders
+    },
   },
   mounted () {
     this.getOrders()
